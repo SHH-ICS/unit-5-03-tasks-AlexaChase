462130c1defde1425e8d6a4744d5acd73ac73a20
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-05-16 14:49:35 +0000

    new changes

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,12 @@
-# Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
+# Create a program that will accept a multiple choice question, four answers, and the letter of correct answer.
 # This will be six lines, and then store the questions in the file questions.txt.
+Question= input("The multiple choice question is:")
+Option1= input("1. ")
+Option2= input("2. ")
+Option3= input("3. ")
+Option4= input("4. ")
+Answer= input("The answer is: ")
+filehandle= open("questions.txt", 'w')
+filehandle.writelines(["\nThe multiple choice question is: ", Question, "\n1. ", Option1,"\n2. ",
+                       Option2, "\n3. ", Option3, "\n4. ", Option4, #Continuation of the code above
+                       "\nPlease type your answer here: ", "\nThe answer is: ", Answer]) #Continuation of the code above
